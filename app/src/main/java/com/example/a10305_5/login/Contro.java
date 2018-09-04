bebf7ba300ea58b65909174bbd6eb8630d5ceb89
a10305-5 <123>
2018-09-04 13:46:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a10305_5/login/Contro.java
@@ -0,0 +1,21 @@
+package com.example.a10305_5.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Contro extends AppCompatActivity {
+
+    Button button2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contro);
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/a10305_5/login/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.a10305_5.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        button=(button)
+    }
+}
